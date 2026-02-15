f1097203a2b78cc1638a0b461a60fbd529258600
ashvin <ashvinanilkumarsh@gmail.com>
2026-02-15 11:07:53 -0600

    Restructure training scripts and add new training script.

--- src/simulation/simulation/walker2d/train_walker_1.py
+++ src/simulation/simulation/walker2d/train_walker_1.py
@@ -5,39 +5,37 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 import torch
 import time
 
-def train():
-    # 1. Configuration
-    env_id = "Walker2d-v4"
-    num_envs = 6   # Number of parallel environments (adjust based on CPU cores)
+
+def train_first_iteration():
+    # Configuration
+    env_id = "Walker2d-v5"
+    num_envs = 12  # Number of parallel environments (adjust based on CPU cores)
     total_timesteps = 4_000_000  # 4 Million steps (needed for a good walker)
-    
-    # 2. Check for GPU
+
+    # Check for GPU
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Training on: {device.upper()}")
 
-    # 3. Create Vectorized Environment
+    # Create Vectorized Environment
     # We use SubprocVecEnv so each env runs on a different CPU core.
-    env = make_vec_env(
-        env_id, 
-        n_envs=num_envs, 
-        vec_env_cls=SubprocVecEnv
-    )
+    env = make_vec_env(env_id, n_envs=num_envs, vec_env_cls=SubprocVecEnv)
 
-    # 4. Initialize the Agent
-    # We use Proximal Policy Optimization(PPO) algorithm here. Earlier we used simple Q-learning.
+    # Initialize the Agent
+    # We use Proximal Policy Optimization(PPO) algorithm here. Earlier 
+    # we used simple Q-learning in Cartpole Agent.
     # I've attached the research paper to PPO here:
     # https://arxiv.org/abs/1707.06347
     # MlpPolicy: Standard Dense Neural Network
     model = PPO(
-        "MlpPolicy", 
-        env, 
-        verbose=1, 
+        "MlpPolicy",
+        env,
+        verbose=1,
         device=device,
         batch_size=2048,  # Larger batch size for GPU efficiency
-        n_steps=2048 // 6 # Divided by 6 because my machine has 6 cores.
+        n_steps=2048 // 12,  # Divided by 12 because my machine has 12 cores.
     )
 
-    # 5. Train
+    # Train
     start_time = time.time()
     print("Starting training...")
     model.learn(total_timesteps=total_timesteps)
@@ -45,12 +43,13 @@ def train():
 
     print(f"Training finished in {(end_time - start_time)/60:.2f} minutes")
 
-    # 6. Save
-    model.save("walker2d_policy")
+    # Save
+    model.save("walker2d_policy_1")
     print("Model saved as 'walker2d_policy.zip'")
-    
+
     # Close the parallel environments
     env.close()
 
-if __name__ == '__main__':
-    train()
+
+if __name__ == "__main__":
+    train_first_iteration()
---
+++ src/simulation/simulation/walker2d/train_walker_2.py
@@ -0,0 +1,73 @@
+import gymnasium as gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+import torch
+import time
+
+
+# Building a Custom wrapper for controlling the x-velocity of the agent
+class TargetVelocityWrapper(gym.RewardWrapper):
+    def __init__(self, env, target_vel=1.0):
+        super().__init__(env)
+        self.target_vel = target_vel
+
+    def reward(self, reward):
+        # Extract velocity from MuJoCo data
+        x_vel = self.env.unwrapped.data.qvel[0]
+
+        # Calculate penalty for deviation
+        velocity_error = abs(x_vel - self.target_vel)
+
+        # New Reward: Survival (+1) minus Error (*2)
+        return 1.0 - (2.0 * velocity_error)
+
+
+def make_env(env_id):
+    env = gym.make(env_id)
+    return TargetVelocityWrapper(env=env, target_vel=1.5)
+
+
+def train_second_iteration():
+    # Configuration
+    env_id = "Walker2d-v5"
+    total_timesteps = 2_000_000  # 4 Million steps (needed for a good walker)
+
+    # Check for GPU
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Training on: {device.upper()}")
+
+    # Create Vectorized Environment
+    # Here, we try to stack the last 4 framees for better learning of where
+    # the legs are.
+    env = DummyVecEnv([make_env(env_id=env_id)])
+    env = VecFrameStack(env, n_stack=4)
+    
+    # Initialize the Agent
+    model = PPO(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        device=device,
+        batch_size=2048,  # Larger batch size for GPU efficiency
+        n_steps=2048,
+    )
+
+    # Train
+    start_time = time.time()
+    print("Starting training...")
+    model.learn(total_timesteps=total_timesteps)
+    end_time = time.time()
+
+    print(f"Training finished in {(end_time - start_time)/60:.2f} minutes")
+
+    # Save
+    model.save("walker2d_policy_2")
+    print("Model saved as 'walker2d_policy_2.zip'")
+
+    # Close the parallel environments
+    env.close()
+
+
+if __name__ == "__main__":
+    train_second_iteration()
